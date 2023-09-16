782922296fbda9605e549714a340b2d169318e02
ivanbyone <ivanbyone@gmail.com>
2023-09-16 13:05:19 +0300

    +two_sum

---
+++ Easy/two_sum.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/two-sum/
+"""
+
+
+class Solution:
+
+    def two_sum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
