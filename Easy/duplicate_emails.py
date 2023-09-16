02124497aeebd261b329e0350dd90c4d7e0b1762
ivanbyone <ivanbyone@gmail.com>
2023-09-16 19:32:03 +0300

    +emails

--- Easy/duplicate_emails.py
+++ Easy/duplicate_emails.py
@@ -8,3 +8,15 @@ import pandas as pd
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
     df = person[person.duplicated(['email'])].drop('id', axis=1)
     return df.drop_duplicates()
+
+
+"""
+https://leetcode.com/problems/delete-duplicate-emails/?lang=pythondata
+"""
+
+# Modify Person in place
+
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id', inplace=True)
+    person.drop_duplicates(subset=['email'], inplace=True)
